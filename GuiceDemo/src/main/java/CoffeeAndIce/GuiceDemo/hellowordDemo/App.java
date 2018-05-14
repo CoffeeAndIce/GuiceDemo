c799522f1643ca6a42a624bb0a9249411dd89ff4
CoffeeAndIce <625740749@qq.com>
2018-05-14 12:28:51 +0800

    1、命令行接受優化

--- GuiceDemo/src/main/java/CoffeeAndIce/GuiceDemo/hellowordDemo/App.java
+++ GuiceDemo/src/main/java/CoffeeAndIce/GuiceDemo/hellowordDemo/App.java
@@ -1,7 +1,12 @@
 package CoffeeAndIce.GuiceDemo.hellowordDemo;
 
+import java.applet.Applet;
+import java.util.Map;
+
+import com.google.common.base.Joiner;
 import com.google.inject.Guice;
 import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 
 /**
@@ -13,11 +18,17 @@ import com.google.inject.name.Names;
  */
 public class App {
 	public static void main(String[] args) {
-		MyApplet mainApplet = Guice.createInjector(new MainModule(),
+		 Map<String, MyApplet> applets = Guice.createInjector(new MainModule(),
 				new CommenLineModule(args))
-				.getInstance(Key.get(
-						MyApplet.class,Names.named(args[0])));
-		mainApplet.run();
+				.getInstance(Key.get(new TypeLiteral<Map<String,MyApplet>>(){}));
+		 if(args.length ==0 || !applets.containsKey(args[0])){
+			 System.out.println("Unable to find applet"
+			 		+ "Valid applets are "
+			 		+ Joiner.on(',').join(applets.keySet()));
+			 return;
+		 }
+		 
+		 applets.get(args[0]).run();
 	}
 
 }
--- GuiceDemo/src/main/java/CoffeeAndIce/GuiceDemo/hellowordDemo/HelloWorldModule.java
+++ GuiceDemo/src/main/java/CoffeeAndIce/GuiceDemo/hellowordDemo/HelloWorldModule.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.name.Names;
+import com.google.inject.multibindings.MapBinder;
 
 
 
 public class HelloWorldModule extends AbstractModule {
 	@Override
 	protected void configure() {
-		bind(MyApplet.class).annotatedWith(Names.named("hello")).to(StringWtringApplet.class);
+		MapBinder.newMapBinder(binder(),String.class, MyApplet.class)
+		.addBinding("hello").to(StringWtringApplet.class);
+//		bind(MyApplet.class).annotatedWith(Names.named("hello")).to(StringWtringApplet.class);
 		bind(MyDestination.class).to(PrintStreamWriter.class);
 		bind(PrintStream.class).toInstance(System.out);
 //		bind(String.class).annotatedWith(OutPut.class).
